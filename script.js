8108c1323d6a8ea7238438772be31a6034a5480c
Chris Bolin <bolin.chris@gmail.com>
2020-10-22 01:25:01 -0600

    add javaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+setInterval(() => console.log("!A FULLY LOADED CITRUS SODA"), 900);
+setInterval(() => console.log("WITH CARBOS >>>"), 1100);
+setInterval(() => console.log("FEED THE RUSH ---"), 1200);
+setInterval(() => console.log("LIFE'S A SCREAM,,,,,,,"), 1300);
+setInterval(() => console.warn("MALTODEXTRIN"), 300);
+setInterval(() => console.error("EXTREME (2)("), 2500);
